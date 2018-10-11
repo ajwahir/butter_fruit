cd44e1c402c162a21688a32dbc8fc78abc57944d
Abhijith <abhijithnbalan@gmail.com>
2018-10-11 20:18:11 +0530

    Matrix Done

--- MatrixGenerator.py
+++ MatrixGenerator.py
@@ -4,7 +4,7 @@ Created on Thu Oct 11 15:37:39 2018
 
 @author: abhij
 """
-# from keras.datasets import boston_housing
+from keras.datasets import boston_housing
 import pandas as pd
 import numpy as np
 from itertools import combinations
@@ -14,27 +14,31 @@ def CombinationGenerator(features,arith):
     combis = []
     upper_limit  = min(5,len(features)+1)
     for i in range(2,upper_limit):
-        sub_arith = arith;
+        sub_arith = arith
+        print(sub_arith)
+        s = len(arith)
         for k in range(i-2):
-            sub_arith = product(sub_arith,arith)
+            p = len(sub_arith)
+            for x in range(p):
+                for y in range(s):
+                    sub_arith.append(sub_arith[x] + arith[y]))
+        # for k in range(i-2):
+        #     for ari in arith:
+        #         sub_arith.append(ari) 
+
         sub = list(combinations(features,i))
-#        s = len(sub)
-#        for c in range(s):
-#            combis.append(product(sub[c],sub_arith))
-        
+        print(list(sub_arith))
         combis.append(list(product(sub,sub_arith)))
-#        sub = list(combinations(features,i))
-#        for k in range(i-1):
-#            for ari in arith:
-#                sub.append(ari)
-#        combis.append(sub)
-    
-    return combis
 
-full_train_data = pd.read_csv('train.csv')
-features = list(full_train_data.columns.values)
-feat = ['A','B','C']
-arith= ['+','*']
+    return combis   
 
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+features = list(range(len(x_train[0])))
+arith = ['+','*']
+feat = ['A','B','C','D']
 
-print(len(CombinationGenerator(feat,arith)))
+all_combination = CombinationGenerator(feat,arith)
+
+for i in range(len(all_combination)):
+    print(len(all_combination[i]))
+
